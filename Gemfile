c69b3750d9d125bc3ad605c45e719fb8e72b7f56
John Smith <assaf@youappi.com>
2014-09-02 17:55:45 +0300

    site generated on 2/9/2014

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+ruby "2.0.0"
+
+gem "bundler"
+gem 'jekyll', '1.5.1'
+gem "jekyll-assets", '0.7.7'
+gem "coffee-script", '2.2.0'
+gem "uglifier", '2.5.0'
+gem "sass", '3.3.5'
+gem "rake", '0.9.6'
+gem "jekyll-import", path: "./jekyll-import-0.2.0"
+gem "sequel", '4.9.0'
+# gem "mysql2", '0.3.15'
+gem "sanitize", '2.1.0'
+
+gem 'pry', '0.9.12.6'
+gem 'pry-remote', '0.1.8'
+gem 'pry-nav', '0.2.3'
---
+++ assets/application-07f972327fcb4602f91f1e8ba4b78498.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+$('document').ready(function() {
+  $("#core-values .double-field-first").click(function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active").parents(".field-item").find(".double-field-second").slideUp();
+    } else {
+      $(this).addClass("active").parents(".field-item").find(".double-field-second").slideDown();
+    }
+  });
+
+  $.scrollUp();
+
+  // enable careers vertiavl carousel
+  Drupal.behaviors.tikalScrollItems.attach();
+  
+  if ($('.jcarousel').length > 0) {
+    var carousel = $('.jcarousel');
+    var slides = carousel.find('li');
+    var numberOfSlides = slides.length;
+    var num = 0;
+    window.setInterval(function () {
+        // increase by num 1, reset to 0 at 4
+        num = (num + 1) % (numberOfSlides);
+        var leftPosition = -(slides.first().width() * num) + "px"; 
+
+        carousel.find('ul').attr('style', 'left: '+leftPosition);
+    }, 4000); // repeat forever, polling every 3 seconds
+  }
+});
